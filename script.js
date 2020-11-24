ce13dcb0d5ef0e21a5763148c0ac235784c755c0
Glitch (justinapupalo-portfolio) <none>
2020-11-24 22:32:30 +0000

    🗾🌦 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,26 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+$(".one").mouseenter(function() {
+  $(".one").css("transform", "scale(3,3)");
+});
+
+$(".one").mouseleave(function() {
+  $(".one").css("transform", "scale(1,1)");
+});
+
+$(".two").mouseenter(function() {
+  $(".two").css("transform", "scale(3,3)");
+});
+
+$(".two").mouseleave(function() {
+  $(".two").css("transform", "scale(1,1)");
+});
+
+$(".three").mouseenter(function() {
+  $(".three").css("transform", "scale(3,3)");
+});
+
+$(".three").mouseleave(function() {
+  $(".three").css("transform", "scale(1,1)");
+});
